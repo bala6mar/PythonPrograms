9273feb3d5414dc5b0e3b97ece5456f217a51ebf
automationbala <40896182+automationbala@users.noreply.github.com>
2018-09-11 18:18:46 +0530

    Leap Year

---
+++ Leap_Year.py
@@ -0,0 +1,20 @@
+year = int(input("Enter any year here: "))
+
+"""if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print ("Leap")
+        else:
+            print("Not a leap")
+    else:
+        print ("Leap")
+else:
+    print("Not a Leap")"""
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap")
+else:
+    print("Not a Leap")
+
+
+
